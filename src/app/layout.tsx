64adc5c5798a0aaf4ab1b6562476aac308bf7c1a
alper <armutdiyar7@gmail.com>
2025-12-23 22:31:20 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script"; // Script bileşenini içe aktarıyoruz
 
 export const metadata: Metadata = {
   title: "Nakliyeci Araçları - Sözleşme, Teklif ve Makbuz Oluşturucu",
@@ -14,6 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google AdSense Kodu */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9395277314770577"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="antialiased">
         {children}
       </body>
